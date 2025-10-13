75d60b883ee284167eddbdb8fb4c5a6d358f9b59
Martin Saposnic <martinsaposnic@gmail.com>
2025-10-13 13:55:17 -0400

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@moneydevkit/lightning-js']
+  serverExternalPackages: ['@moneydevkit/lightning-js'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize lightning-js so it's loaded from node_modules at runtime
+      config.externals = config.externals || [];
+      config.externals.push('@moneydevkit/lightning-js');
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
